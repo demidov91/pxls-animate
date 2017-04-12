53f09e319458e25230f4bab7d20118bf26b078b7
Dzmitry Dziamidau <demidov91@mail.ru>
2017-04-12 14:42:18 +0300

    gif can be generated from the user-defined rectangle

--- run.py
+++ run.py
@@ -20,18 +20,19 @@ def save(out_file: str):
         f.write(data)
 
 
-def to_image(dat_file: str, out_file: str):
-    data = bytes(chain(*data_file_to_rgb_array(dat_file)))
-    Image.frombytes('RGB', defines.DIMENSIONS, data).save(out_file)
+def to_image(dat_file: str, out_file: str, start_point, size):
+    data = bytes(chain(*data_file_to_rgb_array(dat_file, start_point, size)))
+    Image.frombytes('RGB', size, data).save(out_file)
 
 
 def create_gif(
         start_dt: datetime.datetime,
         end_dt: datetime.datetime,
-        rectangle: Tuple[Tuple[int, int], Tuple[int, int]],
+        start_point: Tuple[int, int],
+        size: Tuple[int, int],
         out_file: str):
     frames = (
-        np.array(data_file_to_rgb_array(x), dtype=np.uint8).reshape(defines.DIMENSIONS + (3, ))
+        np.array(list(data_file_to_rgb_array(x, start_point, size)), dtype=np.uint8).reshape((size[1], size[0], 3))
         for x in list_filepaths_in_datetime_range(start_dt, end_dt)[::5]
     )
     imageio.mimwrite(out_file, frames)
@@ -43,16 +44,26 @@ parser.add_argument('--out', type=str, dest='out_file', help='Output file')
 parser.add_argument('--in', type=str, dest='in_file', help='Input file')
 parser.add_argument('--start', type=str, dest='start_dt', help='Start datetime')
 parser.add_argument('--end', type=str, dest='end_dt', help='End datetime')
+parser.add_argument('--start-point', type=int, dest='start_point', nargs=2, help='Left top point of the image.')
+parser.add_argument('--end-point', type=int, dest='end_point', nargs=2, help='Image dimensions. Width and height.')
 
 args = parser.parse_args()
+
+if args.start_dt:
+    start_dt = datetime.datetime.strptime(args.start_dt, defines.API_DATETIME_FORMAT)
+
+if args.end_dt:
+    end_dt = datetime.datetime.strptime(args.end_dt, defines.API_DATETIME_FORMAT)
+
+start_point = args.start_point or (0, 0)
+end_point = args.end_point or tuple(x - 1 for x in defines.DIMENSIONS)
+size = end_point[0] - start_point[0] + 1, end_point[1] - start_point[1] + 1
 
 if args.command == 'save':
     save(args.out_file)
 elif args.command == 'to_image':
-    to_image(args.in_file, args.out_file)
+    to_image(args.in_file, args.out_file, start_point, size)
 elif args.command == 'to_gif':
-    start_dt = datetime.datetime.strptime(args.start_dt, defines.API_DATETIME_FORMAT)
-    end_dt = datetime.datetime.strptime(args.end_dt, defines.API_DATETIME_FORMAT)
-    create_gif(start_dt, end_dt, defines.DIMENSIONS, args.out_file)
+    create_gif(start_dt, end_dt, start_point, size, args.out_file)
 else:
     raise ValueError()
--- utils.py
+++ utils.py
@@ -1,21 +1,17 @@
 import gzip
 import defines
-from typing import Iterable
+from typing import Iterable, Tuple
 import os
 import datetime
 import re
+from contextlib import ContextDecorator
 
 dt_in_filename_pattern = re.compile('(\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2})')
 
 
-def data_file_to_rgb_array(dat_file: str):
-    data = []
-    with gzip.open(dat_file, mode='rb') as f:
-        for compressed in f.read():
-            data.append(defines.PALETTE_BY_BYTE[compressed >> 4])
-            data.append(defines.PALETTE_BY_BYTE[compressed % 16])
-
-    return data
+def data_file_to_rgb_array(dat_file: str, start_point=(0, 0), size=defines.DIMENSIONS) -> Iterable[Tuple[int]]:
+    with compressed_reader(dat_file) as f:
+        yield from f.read_rectangle(start_point, size)
 
 
 def list_filepaths_in_datetime_range(start_dt, end_dt) -> Iterable[str]:
@@ -32,5 +28,43 @@ def list_filepaths_in_datetime_range(start_dt, end_dt) -> Iterable[str]:
         paths.append(os.path.join(defines.DATA_DIR, file_path))
 
     return sorted(paths)
+
+
+
+class compressed_reader(ContextDecorator):
+    extra_encoded_byte = None # type: int
+
+    def __init__(self, dat_file: str):
+        self.dat_file = dat_file
+
+    def __enter__(self):
+        self.fp = gzip.open(self.dat_file, mode='rb')
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.fp.close()
+
+    def read_rectangle(self, start: Tuple[int, int], size: Tuple[int, int]):
+        for row in range(start[1], start[1] + size[1]):
+            row_start = row * defines.DIMENSIONS[0] + start[0]
+            self.fp.seek(row_start // 2)
+            yield from self.read(size[0], more=row_start % 2)
+
+    def read(self, n, more=False):
+        if more:
+            yield defines.PALETTE_BY_BYTE[self.fp.read(1)[0] % 16]
+            n -= 1
+
+        for compressed in self.fp.read(n // 2):
+            yield defines.PALETTE_BY_BYTE[compressed >> 4]
+            yield defines.PALETTE_BY_BYTE[compressed % 16]
+
+        if n % 2:
+            yield defines.PALETTE_BY_BYTE[self.fp.read(1)[0] >> 4]
+
+
+
+
+
 
 
